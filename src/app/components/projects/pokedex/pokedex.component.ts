2df57711b00e1d4e7c683629f3826f7d7488b4b0
frank <sinnigenfrank@gmail.com>
2024-11-15 11:07:06 +0100

    add project pokedex

--- src/app/components/projects/pokedex/pokedex.component.ts
+++ src/app/components/projects/pokedex/pokedex.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from '../../../services/dark-mode.service';
+import { ContextMenuService } from '../../../services/context-menu.service';
 
 @Component({
   selector: 'app-pokedex',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './pokedex.component.scss'
 })
 export class PokedexComponent {
+
+  constructor(public dm: DarkModeService, public cm: ContextMenuService) {}
 
 }
