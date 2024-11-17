29e876566e3da3b6b73e3204269c8c2732e950e6
frank <sinnigenfrank@gmail.com>
2024-11-17 10:23:18 +0100

    change css and scrollTo

--- src/app/services/scroll.service.ts
+++ src/app/services/scroll.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScrollService {
-
-  constructor() { }
+  constructor() {}
 
   scrollToElement(elementId: string): void {
     const element = document.getElementById(elementId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      let offset = 100;
+
+      if (window.innerWidth <= 768) {
+        offset = 50;
+      } else if (window.innerWidth <= 1024) {
+        offset = 75;
+      }
+
+      const elementPosition =
+        element.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({
+        top: elementPosition - offset,
+        behavior: 'smooth',
+      });
     }
   }
 }
